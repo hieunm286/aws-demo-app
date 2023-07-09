c49779c7996eb030c8aa3cd914bd3a2fb6873abd
hieunm286 <hieunm286@gmail.com>
2023-07-09 22:36:20 +0700

    feat: demo task

--- app/page.js
+++ app/page.js
@@ -1,6 +1,44 @@
+'use client'
+
 import Image from 'next/image'
+import axios from "axios";
+import useSWR from "swr";
+import {useState} from "react";
+import {nanoid} from "nanoid";
+
+const fetcher = url => axios.get(url).then(res => res.data)
+
+const Card = (task) => {
+  return (
+      <div className='rounded-lg p-4 border border-solid border-gray-700 w-96'>
+        <p className='text-sm'>{task.id}</p>
+        <p className="mt-3 text-base font-bold">{task.name}</p>
+        <div className='flex flex-col'>
+          <p className='text-green-500'>Status: {task.status}</p>
+          <p className='text-red-500'>Deadline: {task.deadline}</p>
+        </div>
+      </div>
+  )
+}
 
 export default function Home() {
+  const [shouldFetch, setShouldFetch] = useState(false)
+  const { data, isLoading, mutate  } = useSWR(shouldFetch  ? 'https://2inakh1l4i.execute-api.ap-southeast-1.amazonaws.com/v1/task' : null, fetcher)
+
+  const addTask = async () => {
+    try {
+      await axios.post('https://2inakh1l4i.execute-api.ap-southeast-1.amazonaws.com/v1/task', {
+        "id": nanoid(10),
+        "name": `Task ${nanoid(10)}`,
+        "status": "To do",
+        "deadline": "2023-14-05T14:48:00.000Z"
+      })
+      await mutate()
+    } catch (err) {
+      console.log(err.message)
+    }
+  }
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
@@ -28,7 +66,7 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
+      <div className="relative flex flex-col gap-10 place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
         <Image
           className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
           src="/next.svg"
@@ -37,6 +75,21 @@ export default function Home() {
           height={37}
           priority
         />
+        <div className="flex gap-3 mt-6 z-10"><button className='w-40 h-10 bg-blue-700 rounded-2xl cursor-pointer hover:bg-blue-800' onClick={() => setShouldFetch(true)}>
+          GET ALL TASKS
+        </button>
+          <button className='w-40 h-10 bg-blue-700 rounded-2xl cursor-pointer hover:bg-blue-800' onClick={addTask}>
+            ADD A TASKS
+          </button></div>
+        {
+          isLoading ? <p className='text-white'>Fetching task...</p> : (
+              <div className='flex gap-6 flex-wrap items-center justify-center'>
+                {
+                  data?.Items.map(item => <Card key={item.id} {...item} />)
+                }
+              </div>
+          )
+        }
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
